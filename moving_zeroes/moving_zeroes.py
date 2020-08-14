c263dc9aff48c16fb855e4de9339571795933dbf
Spencer Adams <spenceradams@Spencers-MacBook-Pro.local>
2020-08-14 00:50:56 -0500

    moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,21 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    border = 0
+
+    for ii in range(len(arr)):
+        if arr[ii] != 0:
+            # if the number isn't 0 then switch to the border
+            arr[ii], arr[border] = arr[border], arr[ii]
+
+        # increment the border
+        if arr[border] != 0:
+            border += 1
+
+    return arr
 
 
 if __name__ == '__main__':
